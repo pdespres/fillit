47d99d9ef77f9ee096a7c83a39016a514eba6f60
Paul DESPRES <pdespres@e3r4p10.42.fr>
2017-11-16 18:59:50 +0100

    day 3

--- algo.c
+++ algo.c
@@ -6,7 +6,7 @@
 /*   By: pdespres <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/15 21:18:51 by pdespres          #+#    #+#             */
-/*   Updated: 2017/11/16 16:30:44 by pdespres         ###   ########.fr       */
+/*   Updated: 2017/11/16 18:59:26 by pdespres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,20 @@ int		check_block(char **map, t_list tetri, int maxsize, int piece)
 	}
 	x += (piece > 0 ? tetri->p[piece][0] : 0);
 	y += (piece > 0 ? tetri->p[piece][1] : 0);
+	if (x < 0 || x > maxsize || y < 0 || y > maxsize)
+		return (0);
 	if (map[x][y] == EMPTY)
-	{
+	{	
 		if (piece == 3 || (piece < 3 && check_block(map, tetri, maxsize, piece + 1)))
 		{
-			map[x][y] = (tetri->num - 1 + 'A');
+			map[x][y] = (tetri->num + 'A');
 			return (1)
 		}
 	}
 	return (0);
 }
 
-int		put_tetri(char **map, t_list *tetri, int maxsize)
+int		put_tetri(char **map, t_list *tetri, int maxsize, int decalage)
 {
 	char	x;
 	char	y;
@@ -48,13 +50,16 @@ int		put_tetri(char **map, t_list *tetri, int maxsize)
 		{
 			if (map[x][y] == EMPTY)
 			{
-				tetri->p[0][0] = x;
+				if (decalage == 0)
+					tetri->p[0][0] = x;
 				tetri->p[0][1] = y;
 				if (check_block(map, tetri, maxsize, 0))
 				{
-					map[x][y] = (tetri->num - 1 + 'A');
+					map[x][y] = (tetri->num + 'A');
 					return (1);
 				}
+				else
+					decalage--;
 			}
 		}
 	}
@@ -64,16 +69,18 @@ int		put_tetri(char **map, t_list *tetri, int maxsize)
 int		resolve(char **map, t_list *tetri, int maxsize)
 {
 	t_list	*temp;
+	int		decalage;
 
 	temp = tetri;
+	decalage = 0;
 	if (put_tetri(map, temp, maxsize))
 	{
-		temp = temp->next;
-		if (temp = NULL)
+		if (temp->next = NULL)
 			return (1);
-		if (!resolve(map, temp, maxsize))
+		while (!resolve(map, temp->next, maxsize))
 		{
 			clear_tetri();
+			decalage++;
 		}
 	}
 	else
