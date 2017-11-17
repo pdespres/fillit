0ce1e02b3f36865096066ae6697867f769d05224
Paul DESPRES <pdespres@e3r3p7.42.fr>
2017-11-17 10:20:15 +0100

    day 4

--- main.c
+++ main.c
@@ -6,11 +6,17 @@
 /*   By: pdespres <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 14:55:54 by pdespres          #+#    #+#             */
-/*   Updated: 2017/11/16 09:53:47 by pdespres         ###   ########.fr       */
+/*   Updated: 2017/11/17 10:19:47 by pdespres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+/*	Rules:																	*/
+*/	l'ensemble des tetri est un tab de chaine[4] avec indice = ordre		*/
+/*	un tetri est une chaine[4] avec les 4 pos des blocs max 255				*/
+*/	creation tetri: chaine[0] = 0, chaine[1-3] = +X sur carre de 4			*/
+*/	map: carre de cote X sous forme de chaine de longueur X*X				*/
 
 int			main(int ac, char **av)
 {
