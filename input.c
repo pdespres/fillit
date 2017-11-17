aac7cc7a095ba5b4a971370b5181db9b804d0e47
Niels <32128732+NielsCohen@users.noreply.github.com>
2017-11-17 15:20:43 +0100

    niels update segfault

--- input.c
+++ input.c
@@ -6,7 +6,7 @@
 /*   By: pdespres <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 21:33:44 by pdespres          #+#    #+#             */
-/*   Updated: 2017/11/17 12:58:20 by ncohen           ###   ########.fr       */
+/*   Updated: 2017/11/17 15:04:11 by ncohen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,50 +19,77 @@ char	**check_tetri(char *str)
 	char	**str_places;
 	int		piece_number;
 	int		first_place;
+	int		refresh;
+	int		x;
+
+	x = 0;
 	i = 0;
 	j = 1;
 	while(str[i])
 		i++;
 	piece_number = i / 21;
+	ft_putnbr(piece_number);
 	i = 0;
-	str_places = (char**)malloc(sizeof(*str_places) * (piece_number + 2));
+	if(!(str_places = (char**)malloc(sizeof(*str_places) * (piece_number + 2))))
+		return (NULL);
 	while (piece_number + 2 != i)
-		str_places[i++] = (char*)malloc(sizeof(**str_places) * 5);
-	str_places[i][0] = 0;
+	{
+		if(!(str_places[i] = (char*)malloc(sizeof(**str_places) * 5)))
+			return (NULL);
+		i++;
+	}
+	str_places[i - 1][0] = 0;
 	i = 1;
+	refresh = 1;
 	while(str[x])
 	{
 		while (str[x] != '#')
+		{
 			x++;
 			refresh++;
+		}
 		if (refresh < 5)
-			piece_number[i][j] = refresh;
-			first_place = refresh;		
+		{
+			str_places[i][j] = refresh;
+			first_place = refresh;
+		}
 		else if(refresh > 5 && refresh < 10)
+		{
 			str_places[i][j] = refresh - 5;
 			first_place = refresh - 5;
+		}
 		else if (refresh > 10 && refresh < 15)
+		{
 			str_places[i][j] = refresh - 10;
 			first_place = refresh - 10;
+		}
 		else if (refresh > 15 && refresh < 20)
+		{
 			str_places[i][j] = refresh - 15;
 			first_place = refresh - 15;
+		}
 		j++;
 		while(str[x] != '\n' && str[x + 1] != '\n')
 		{
+			write(1, "t\n", 2);
 			x++;
+			refresh++;
 			if (str[x] == '#')
-				str_places[i][j++] = refresh - first_place;
+			{
+				str_places[i][j] = refresh - first_place;
+				j++;
+			}
 		}
+		write(1, "\n", 1);
 		x += 2;
 		i++;
 		j = 1;
 		refresh = 0;
 	}
-	str_place[i][j - 1] = 0;
-	return (str_place);
+	str_places[i][j - 1] = 0;
+	return (str_places);
 }
-
+/*
 int		check_file(char *str)
 {
 	int		i;
@@ -104,4 +131,4 @@ t_tetri	*open_file(char *file)
 	error((str == NULL));
 	error((check_file(str) == 0));
 	return (check_tetri(str));
-}
+}*/
