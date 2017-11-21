d003b81b0114bb191961af91292b3f31ac2d03af
Paul DESPRES <pdespres@e3r2p6.42.fr>
2017-11-21 12:57:00 +0100

    day 6

--- load_file.c
+++ load_file.c
@@ -6,58 +6,11 @@
 /*   By: pdespres <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 18:32:10 by pdespres          #+#    #+#             */
-/*   Updated: 2017/11/20 19:59:57 by pdespres         ###   ########.fr       */
+/*   Updated: 2017/11/21 12:56:33 by pdespres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-
-/*
-char		**check_tetri(char **str)
-{
-	int 	i;
-	char	**str_places;
-	int		decalage;
-	int		x;
-
-	i = 0;
-	while(str[0][i])
-	{
-		i++;
-	}
-	x = i / 20 - (i / 20) / 20;
-	if(!(str_places = (char**)malloc(sizeof(*str_places) * (x + 2))))
-		return (NULL);
-	str_places[x + 1] = 0;
-	i = 0;
-	while (i <= x)
-	{
-		if(!(str_places[i] = (char*)malloc(sizeof(**str_places) * 5)))
-			return (NULL);
-		i++;
-	}
-	i = 0;
-	x = 1;
-	decalage = 0;
-	while(str[0][i])
-	{
-		if (i % 21 == 0)
-		{
-			x = 1;
-			decalage = 0;
-		}
-		if (str[0][i] == FULL)
-		{
-			if (x == 1)
-				decalage = (i - (i / 21) * 21) / 5 * 5 + (i / 21 * 21);
-			str_places[i / 21 + 1][x] = i - decalage;
-			x++;
-		}
-		i++;
-	}
-	return (str_places);
-}
-*/
 
 static char	*read_file(int fd)
 {
@@ -87,16 +40,12 @@ static char	*read_file(int fd)
 	return (str);
 }
 
-static int	check_file(char *str)
+static int	check_file(char *str, int bump, int nfull)
 {
 	int		i;
-	int		bump;
-	int		nfull;
 
 	i = 0;
-	bump = 0;
-	nfull = 0;
-	while (1)
+	while (str[i++] != '\0')
 	{
 		if ((i + 1 - bump) % 5 == 0)
 		{
@@ -116,9 +65,6 @@ static int	check_file(char *str)
 		}
 		bump = (i) / 21;
 		nfull += (str[i] == FULL ? 1 : 0);
-		i++;
-		if (str[i] == '\0')
-			break ;
 	}
 	return ((i > 0 && str[i - 1] == '\n') && (i + 1) % 21 == 0);
 }
@@ -127,14 +73,18 @@ char		**open_file(char *file, char *str)
 {
 	int		fd;
 	char	**tetri;
+	int		bump;
+	int		nfull;
 
+	bump = 0;
+	nfull = 0;
 	ft_error((file == NULL));
 	fd = open(file, O_RDONLY);
 	ft_error((fd < 0));
 	str = read_file(fd);
 	ft_error((str == NULL));
 	ft_error(close(fd) == -1);
-	ft_error((check_file(str) == 0));
+	ft_error((check_file(str, bump, nfull) == 0));
 	tetri = check_tetri(str);
 	ft_error(tetri == NULL);
 	return (tetri);
